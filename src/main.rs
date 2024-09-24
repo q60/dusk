3f69e8c4c4619fca491d7c705e86b8fcf0a79123
Yuki Sireneva <yuki.utk8g@gmail.com>
2024-09-24 23:27:49 +0300

    chrono -> 0.4; edition -> 2021; clippy; remove redundant functions

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,27 @@
-use chrono::{Datelike, Offset, Timelike};
+use chrono::{Datelike, Timelike};
 use std::f64::consts::PI;
 
 fn main() {
     let time = chrono::Local::now();
 
-    let timezone_offset = time.offset().local_minus_utc().num_minutes() as f64;
+    let timezone_offset = time.offset().local_minus_utc() as f64 / 60.;
     let year = time.year();
     let day_of_the_year = time.ordinal() as f64;
     let month = time.month();
     let day = time.day();
     let hour = time.hour() as f64;
+    let days = if time.naive_utc().date().leap_year() {
+        366
+    } else {
+        365
+    };
 
-    let latitude_deg = 51.671667;
-    let longitude_deg = 39.210556;
-    let latitude_rad = deg_to_rad(latitude_deg);
+    let latitude_deg: f64 = 51.671667;
+    let longitude_deg: f64 = 39.210556;
+    let latitude_rad = latitude_deg.to_radians();
 
     // fractional year in radians
-    let g = 2. * PI / (days(year) as f64) * (day_of_the_year - 1. + (hour - 12.) / 24.);
+    let g = 2. * PI / (days as f64) * (day_of_the_year - 1. + (hour - 12.) / 24.);
 
     // equation of time in minutes
     let eqtime = 229.18
@@ -32,17 +37,17 @@ fn main() {
         + 0.00148 * (3. * g).sin();
 
     // hour angle in radians
-    let ha = (deg_to_rad(90.833).cos() / (latitude_rad.cos() * decl.cos())
+    let ha = (90.833_f64.to_radians().cos() / (latitude_rad.cos() * decl.cos())
         - latitude_rad.tan() * decl.tan())
     .acos();
 
     // sunrise time in minutes
-    let sunrise = (720. - 4. * (longitude_deg + rad_to_deg(ha)) - eqtime) + timezone_offset;
+    let sunrise = (720. - 4. * (longitude_deg + ha.to_degrees()) - eqtime) + timezone_offset;
     let sunrise_hour = (sunrise / 60.) as i32;
     let sunrise_minutes = (sunrise - (sunrise_hour as f64) * 60.) as i32;
 
     // sunset time in minutes
-    let sunset = (720. - 4. * (longitude_deg - rad_to_deg(ha)) - eqtime) + timezone_offset;
+    let sunset = (720. - 4. * (longitude_deg - ha.to_degrees()) - eqtime) + timezone_offset;
     let sunset_hour = (sunset / 60.) as i32;
     let sunset_minutes = (sunset - (sunset_hour as f64) * 60.) as i32;
 
@@ -58,20 +63,4 @@ fn main() {
         " \x1B[93m-\x1B[0m sunset: {:0>2}:{:0>2}",
         sunset_hour, sunset_minutes,
     );
-}
-
-fn deg_to_rad(angle_deg: f64) -> f64 {
-    PI * angle_deg / 180.
-}
-
-fn rad_to_deg(angle_rad: f64) -> f64 {
-    180. * angle_rad / PI
-}
-
-fn days(year: i32) -> i32 {
-    if (year % 4 == 0) && (year % 100 != 0 || (year % 100 == 0 && year % 400 == 0)) {
-        return 366;
-    } else {
-        return 365;
-    };
 }
